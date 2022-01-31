b57c86101c0e8bfb1df1fef399ed82e9981c3fc9
Tashiro <g4160hc@gmail.com>
2022-01-31 10:24:48 +0900

    support elixir version

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule ExAws.Dynamo.Mixfile do
     [
       app: :ex_aws_dynamo,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
